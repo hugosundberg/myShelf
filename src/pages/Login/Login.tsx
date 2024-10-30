ab8be88998009c14b6508f17141ca712ec13e466
hugosundberg <hugosundberg98@gmail.com>
2024-10-30 14:47:38 +0100

    firebase

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,13 +2,17 @@ import styles from "./Login.module.css";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../utils/firebase.js";
 
 const Login: React.FC = () => {
+  // Sign in with google
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
-      const result = await signInWithPopup(googleProvider, auth);
-    } catch (error) {}
+      const result = await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
