ebeb401d02c945cf29404d753e0ffc7f084a71ef
hugosundberg <hugosundberg98@gmail.com>
2024-10-30 14:43:29 +0100

    auth start

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,22 @@
 import styles from "./Login.module.css";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const Login: React.FC = () => {
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(googleProvider, auth);
+    } catch (error) {}
+  };
+
   return (
     <>
       <div className={styles.contentBody}>
         <div className={styles.loginContainer}>
           <h2>Login</h2>
+          <p>Sign in using one of the providers</p>
           <button className={styles.loginButton}>
             <h3>
               <FcGoogle className={styles.googleIcon} />
