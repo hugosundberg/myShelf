ecacc5b212167a5a0719338bfa36415a33c32a2c
hugosundberg <hugosundberg98@gmail.com>
2024-11-28 15:45:39 +0100

    shelves

--- src/pages/MyBooks/MyBooks.tsx
+++ src/pages/MyBooks/MyBooks.tsx
@@ -6,6 +6,8 @@ import { collection, getDocs } from "firebase/firestore";
 import { SmallBookCard } from "../../components/SmallBookCard/SmallBookCard";
 import { CircularProgress } from "@mui/material";
 import Sort from "../../components/SortComponent/Sort";
+import shelf from "../../assets/shelf.webp";
+import React from "react";
 
 interface MyBooksProps {
   setCurrentBook: (book: Book) => void;
@@ -22,6 +24,30 @@ const MyBooks: React.FC<MyBooksProps> = ({
   const [loading, setLoading] = useState(false);
   const [sortOption, setSortOption] = useState<string>(""); // Sort state
   const [sortDirection, setSortDirection] = useState<string>("");
+  const [booksPerRow, setBooksPerRow] = useState(3);
+
+  useEffect(() => {
+    const updateBooksPerRow = () => {
+      if (window.innerWidth > 2428) {
+        setBooksPerRow(6);
+      } else if (window.innerWidth > 1820) {
+        setBooksPerRow(5); // Large screens
+      } else if (window.innerWidth > 1455) {
+        setBooksPerRow(4);
+      } else if (window.innerWidth > 1091) {
+        setBooksPerRow(3); // Tablets
+      } else if (window.innerWidth > 727) {
+        setBooksPerRow(2); // Small screens
+      } else {
+        setBooksPerRow(1);
+      }
+    };
+
+    updateBooksPerRow(); // Set initially
+    window.addEventListener("resize", updateBooksPerRow);
+
+    return () => window.removeEventListener("resize", updateBooksPerRow);
+  }, []);
 
   useEffect(() => {
     const fetchUserBooks = async () => {
@@ -103,20 +129,32 @@ const MyBooks: React.FC<MyBooksProps> = ({
 
         <div className={styles.collectionBody}>
           {sortedLikedBooks.length > 0 ? (
-            sortedLikedBooks.map((book, index) => (
-              <SmallBookCard
-                key={`${book.id}-${index}`}
-                book={book}
-                setCurrentBook={setCurrentBook}
-                setCurrentAuthor={setCurrentAuthor}
-              />
-            ))
+            <>
+              {sortedLikedBooks.map((book, index) => {
+                return (
+                  <React.Fragment key={`${book.id}-${index}`}>
+                    <SmallBookCard
+                      book={book}
+                      setCurrentBook={setCurrentBook}
+                      setCurrentAuthor={setCurrentAuthor}
+                    />
+                    {(index + 1) % booksPerRow === 0 && (
+                      <img src={shelf} className={styles.shelf} alt="" />
+                    )}
+                  </React.Fragment>
+                );
+              })}
+              {sortedLikedBooks.length % 3 !== 0 && (
+                <img src={shelf} className={styles.shelf} alt="" />
+              )}
+            </>
           ) : (
             <h3>
               You have no saved books. Try searching for one of your favorites!
             </h3>
           )}
         </div>
+
         {sortedRatedBooks.length > 0 && (
           <>
             <h2>Not in your collection</h2>
